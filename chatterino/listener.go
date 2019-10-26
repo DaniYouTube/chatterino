2c261cf40c1f1ffbc0062d5091e8fd93424fe514
Sergi Vos <sergivb01@gmail.com>
2019-10-27 01:31:57 +0200

    move handlers to separate file, cleanup kinda messy code

---
+++ chatterino/listener.go
@@ -0,0 +1,80 @@
+package chatterino
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func (srv *ChatServer) sendMessages() {
+	for {
+		msg := <-srv.messages
+		rawMsg := []byte(msg)
+
+		srv.mu.RLock()
+		for conn, id := range srv.clients {
+			if _, err := conn.Write(rawMsg); err != nil {
+				fmt.Printf("error sending %q to user id %d (%s): %s\n", msg, id, conn.RemoteAddr().String(), err)
+			}
+		}
+		srv.mu.RUnlock()
+		fmt.Printf("[CHAT] %s", msg)
+	}
+}
+
+func (srv *ChatServer) readMessages(conn *net.Conn, id string) {
+	rdr := bufio.NewReader(*conn)
+	for {
+		msg, err := rdr.ReadString('\n')
+		if err != nil {
+			srv.deadConns <- *conn
+			break
+		}
+		srv.messages <- fmt.Sprintf("[%s]: %s", id, msg)
+	}
+}
+
+func (srv *ChatServer) handleConnections(ln *net.Listener) error {
+	go func(ln *net.Listener) {
+		for {
+			conn, err := (*ln).Accept()
+			if err != nil {
+				fmt.Printf("couldn't accept new conn: %v\n", err)
+			}
+			srv.newConns <- conn
+		}
+	}(ln)
+
+	for {
+		select {
+		case conn := <-srv.newConns:
+			id := randomSha1(conn.RemoteAddr().String())
+
+			// block mutex to avoid data race on conn & id storing
+			srv.mu.Lock()
+			srv.clients[conn] = id
+			srv.mu.Unlock()
+
+			go srv.readMessages(&conn, id)
+
+			fmt.Printf("accepted new client %s from %s!\n", id, conn.RemoteAddr().String())
+			srv.messages <- fmt.Sprintf("Hit the dabs for %s, who just joined Chatterino! \\o> <o/"+ChatEOL, id)
+			break
+
+		case deadConn := <-srv.deadConns:
+			id := srv.clients[deadConn]
+
+			fmt.Printf("user %s (%s) disconnected!\n", id, deadConn.RemoteAddr().String())
+			srv.messages <- fmt.Sprintf("User %s is no longer with us :("+ChatEOL, id)
+
+			srv.mu.Lock()
+			delete(srv.clients, deadConn)
+			srv.mu.Unlock()
+			break
+
+		case <-srv.quit:
+			srv.messages <- "Bye...! Chatterino is being shutdown..." + ChatEOL
+			return (*ln).Close()
+		}
+	}
+}
--- chatterino/server.go
+++ chatterino/server.go
@@ -1,7 +1,6 @@
 package chatterino
 
 import (
-	"bufio"
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
@@ -27,6 +26,17 @@ type ChatServer struct {
 	mu sync.RWMutex
 }
 
+func (srv *ChatServer) Listen(addr string) error {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("chatterino started listening on %s!\n", addr)
+
+	go srv.sendMessages()
+	return srv.handleConnections(&ln)
+}
+
 func NewServer() *ChatServer {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
@@ -45,85 +55,4 @@ func randomSha1(addr string) string {
 	h.Write([]byte(addr))
 	rawHash := base64.URLEncoding.EncodeToString(h.Sum(nil))
 	return rawHash[:len(rawHash)-1]
-}
-
-func (srv *ChatServer) handleMessages() {
-	for {
-		msg := <-srv.messages
-		rawMsg := []byte(msg)
-
-		srv.mu.RLock()
-		for conn, id := range srv.clients {
-			if _, err := conn.Write(rawMsg); err != nil {
-				fmt.Printf("error sending %q to user id %d (%s): %s\n", msg, id, conn.RemoteAddr().String(), err)
-			}
-		}
-		srv.mu.RUnlock()
-		fmt.Printf("[CHAT] %s", msg)
-	}
-}
-
-func (srv *ChatServer) acceptConns(ln *net.Listener) {
-	for {
-		conn, err := (*ln).Accept()
-		if err != nil {
-			fmt.Printf("couldn't accept new conn: %v\n", err)
-		}
-		srv.newConns <- conn
-	}
-}
-
-func (srv *ChatServer) Listen(addr string) error {
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("chatterino started listening on %s!\n", addr)
-
-	go srv.acceptConns(&ln)
-	go srv.handleMessages()
-
-	for {
-		select {
-		case conn := <-srv.newConns:
-			id := randomSha1(conn.RemoteAddr().String())
-
-			// block mutex to avoid data race on conn & id storing
-			srv.mu.Lock()
-			srv.clients[conn] = id
-			srv.mu.Unlock()
-
-			go func(conn net.Conn, id string) {
-				rdr := bufio.NewReader(conn)
-				for {
-					msg, err := rdr.ReadString('\n')
-					if err != nil {
-						srv.deadConns <- conn
-						break
-					}
-					srv.messages <- fmt.Sprintf("[%s]: %s", id, msg)
-				}
-			}(conn, id)
-
-			fmt.Printf("accepted new client %s from %s!\n", id, conn.RemoteAddr().String())
-			srv.messages <- fmt.Sprintf("Hit the dabs for %s, who just joined Chatterino! \\o> <o/"+ChatEOL, id)
-			break
-
-		case deadConn := <-srv.deadConns:
-			id := srv.clients[deadConn]
-
-			fmt.Printf("user %s (%s) disconnected!\n", id, deadConn.RemoteAddr().String())
-			srv.messages <- fmt.Sprintf("User %s is no longer with us :("+ChatEOL, id)
-
-			srv.mu.Lock()
-			delete(srv.clients, deadConn)
-			srv.mu.Unlock()
-
-			break
-
-		case <-srv.quit:
-			srv.messages <- "Bye...! Chatterino is being shutdown..." + ChatEOL
-			return ln.Close()
-		}
-	}
 }
